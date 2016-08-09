038049dea912ff5b2a434e0bf9d1c2655be51f0c
Zach Marcantel <zmarcantel@gmail.com>
2016-08-08 19:34:31 -0500

    additional mcu generator docs

--- src/mcu/mod.rs
+++ src/mcu/mod.rs
@@ -8,21 +8,26 @@ mcu!(
     ];
 
     constants => {
-        i2c_loc => 0x8000;
+        i2c_loc = 0x8000;
     };
 
     // TODO: externs sections
 
-    interrupts => {
+    interrupts => [255] @ .interrupts {     // NOTE: argument to @ _must_ be a link section
         0       => main;
         1..5    => some_common_handler;
         6       => another_handler;
         7..127  => None;
     };
 
+    // NOTE: the `base` field requires a link section to place the base pointer -- generally right before the IVT
+    //       the stack **will not** be located at the link section, simply a pointer to it.
+    //       it is entirely reasonable to have something like:
+    //          `base => .stack_begin @ .stack_ptr`
+    //       this would place the stack base at `.stack_begin` and create a constant STACK_BEGIN
     stack => {
-        base    => 0x1000;
-        limit   => STACK_LIMIT; // externally defined const. could easily be an internal constant
+        base    => 0x1000 @ .stack_ptr;
+        limit   => STACK_LIMIT;             // externally defined const. could also be an internal constant or literal
     };
 
     data => {
@@ -36,6 +41,48 @@ mcu!(
         uart    => uart::UART @ UART_1;         // can also be a const/extern
         i2c     => i2c::I2C @ i2c_loc;          // can also be an internal constant
     };
+
+    actions => [
+        // NOTE: functions with empty () args after the ident get an implicit `&mut self`
+        //
+        // NOTE: actions cannot have return values
+        //
+        // NOTE: init is a special cased action.
+        //       if it is defined we will not generate one -- if it is not defined, the generated MCU::init() would
+        //          1. unlock the watchdog (assumes self.wdog.unlock() exists)
+        //          2. disable the watchdog (assumes self.wdog.disable() exists)
+        //          3. memcpy the .data section to RAM using generated copy_data_section() method
+        //
+        // NOTE: this function should be the first thing called in the reset_handler.
+        //       from there, additional peripherals can be setup and/or app-code-jumping can be implemented
+        init() => {
+            // use a local peripheral to unlock the wdog and disable it
+            self.wdog.unlock();
+            self.wdog.disable()
+
+            // use the generated copy_data_section() function to copy the .data section to RAM
+            self.copy_data_section();
+
+            // use a self-defined action in another action!!!
+            self.enable_hardfloat();
+        };
+
+
+        // NOTE: this function also has implicit self
+        enable_hardfloat() => {
+            self.my_periph.enable();
+        }
+
+        // sets the given pin to digital mode.
+        // just an example -- so probably not realistic
+        digital_pin(pin: u8) =>() {
+            match pin / PINS_PER_PORT {
+                1 => { self.port_a.digital(pin % PINS_PER_PORT); }
+                2 => { self.port_a.digital(pin % PINS_PER_PORT); }
+                _ => { panic!("could not find pin"); }
+            }
+        }
+    ];
 );
 
 */
