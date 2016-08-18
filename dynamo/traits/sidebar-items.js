49897fb8830f8e0c20ff8e637cfbd779a00cc323
Travis-Generated Documentation <dynamo@travis-ci.org>
2016-08-18 11:23:08 +0000

    doc upload for dynamo (3535c7c4b1f279883ccf2523bc5cc093ae66f4d8)

--- dynamo/traits/sidebar-items.js
+++ dynamo/traits/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["MCU",""],["NVIC",""],["Peripheral",""]]});
+initSidebarItems({"trait":[["MCU","Enables generic handling and initialization of an MCU."],["NVIC","Standard interface to the Nested Vector Interrupt Controller."],["Peripheral","Trait all ioreg (or equivalent) peripherals need to implement if they wish to be handled generically."]]});
